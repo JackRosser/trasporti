e2e82983652b0357c69b37bebdcf92c6f8035c60
Giacomo Rossettini <rossegiac@gmail.com>
2024-12-11 11:00:24 +0100

    navbar complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'backoffice', loadChildren: () => import('./pages/backoffice/backoffice.module').then(m => m.BackofficeModule) }
 ];
 
 
