45fe321a79a048a1d14ad0a3833f077b0de69cd6
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-12-13 09:15:21 +0100

    aggiornamenti rotta utenti e admin

--- src/app/pages/backoffice/inserimento-mezzi/inserimento-mezzi.component.ts
+++ src/app/pages/backoffice/inserimento-mezzi/inserimento-mezzi.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MezziService } from '../../../services/mezzi.service';
 import { iMezzo } from '../../../interfaces/i-mezzo';
 import { eStato } from '../../../interfaces/e-stato';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inserimento-mezzi',
@@ -11,19 +12,15 @@ import { eStato } from '../../../interfaces/e-stato';
 export class InserimentoMezziComponent implements OnInit {
   formData: {
     tipo: string; // Aggiungi questa proprietà
-    seatQuantity: number;
-    readyToCirculate: string;
-    code: string;
+    codice: number;
   } = {
     tipo: '', // Inizializza come stringa vuota
-    seatQuantity: 0,
-    readyToCirculate: '',
-    code: '',
+    codice: 0,
   };
 
   mezzi: iMezzo[] = []; // Array per memorizzare i mezzi recuperati dal server
 
-  constructor(private mezziService: MezziService) {}
+  constructor(private mezziService: MezziService, private router: Router) {}
 
   ngOnInit(): void {
     this.getMezzi(); // Recupera i mezzi all'inizializzazione del componente
@@ -31,55 +28,27 @@ export class InserimentoMezziComponent implements OnInit {
 
   // Metodo per inviare il form
   onSubmit(formValue: any): void {
-    if (!formValue.seatQuantity || formValue.seatQuantity <= 0) {
-      alert('La quantità di posti a sedere deve essere un valore positivo.');
-      return;
-    }
-
-    if (!formValue.readyToCirculate) {
-      alert('Si prega di selezionare se è pronto a circolare.');
-      return;
-    }
-
-    if (!formValue.code) {
-      alert('Il campo Codice è obbligatorio.');
-      return;
-    }
-
-    // Mappa il valore booleano a un valore dell'enumerazione `eStato`
-    const stato =
-      formValue.readyToCirculate === 'true'
-        ? eStato.IN_SERVIZIO
-        : eStato.DEPOSITO;
-
-    const mezzo: Partial<iMezzo> = {
-      capienza: formValue.seatQuantity,
-      stato: stato, // Usa il valore mappato di `eStato`
-      codice: Number(formValue.code),
-      tipo: formValue.tipo, // Passa il tipo selezionato
-    };
-
-    if (!mezzo.tipo) {
+    if (!this.formData.tipo) {
       alert('Il tipo del mezzo è obbligatorio.');
       return;
     }
 
-    if (!mezzo.codice) {
+    if (this.formData.codice === 0) {
       alert('Il tipo del mezzo è obbligatorio.');
       return;
     }
 
-    this.mezziService.createMezzo(mezzo.tipo, mezzo.codice).subscribe(
-      (response) => {
-        console.log('Risposta dal server:', response);
-        alert('Mezzo aggiunto con successo!');
-        this.getMezzi(); // Aggiorna la lista dei mezzi
-      },
-      (error) => {
-        console.error('Errore:', error);
-        alert("Errore durante l'aggiunta del mezzo.");
-      }
-    );
+    this.mezziService
+      .createMezzo(this.formData.tipo, this.formData.codice)
+      .subscribe(
+        (response) => {
+          alert('Mezzo aggiunto con successo!');
+          this.getMezzi(); // Aggiorna la lista dei mezzi
+        },
+        (error) => {
+          alert('Errore nella richiesta!');
+        }
+      );
   }
 
   // Metodo per recuperare tutti i mezzi
@@ -88,6 +57,7 @@ export class InserimentoMezziComponent implements OnInit {
       (response) => {
         console.log('Mezzi recuperati:', response);
         this.mezzi = response;
+        this.router.navigate(['/parco-mezzi']);
       },
       (error) => {
         console.error('Errore durante il recupero dei mezzi:', error);
